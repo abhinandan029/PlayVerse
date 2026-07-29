21da7152b0396e4a40a7a2eb241318f3881fbe69
Abhinandan Manakapure <abhinandanaj29@gmail.com>
2026-07-30 01:49:31 +0530

    Layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import {Routes, Route} from 'react-router-dom'
+
 import Header from "./components/Header.jsx"
+import Body from "./components/Body.jsx"
 
 function App() {
 
   return (
     <>
-      
+      <Header />
+      <Routes>
+        <Route path="/home" element={<Body />}/>
+      </Routes>
     </>
   );
   
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import {useNavigate} from 'react-router-dom'
+
+function Header() {
+  const navigate = useNavigate();
+
+  return (
+    <div 
+    className="flex gap-2 border-none rounded-xl py-2 px-4 justify-between items-center bg-white/10 m-2 backdrop-filter-xs sticky top-2 left-0 right-0">
+      
+      <span className="text-[30px] font-mono font-bold text-white">PlayVerse</span>
+      
+      <div className="flex gap-2 text-white text-[18px]">
+        
+        <button 
+        className="px-2 hover:bg-black/40 rounded-md cursor-pointer transition-all duration-300 ease-in-out"
+        onClick={() => navigate("/home")}>Home</button>
+        <button className="px-2 hover:bg-black/40 rounded-md cursor-pointer transition-all duration-300 ease-in-out">Wishlist</button>
+        <button className="px-2 hover:bg-black/40 rounded-md cursor-pointer transition-all duration-300 ease-in-out">About Us</button>
+        <button className="px-2 hover:bg-black/40 rounded-md cursor-pointer transition-all duration-300 ease-in-out">Notifications</button>
+        <button className="px-2 hover:bg-black/80 rounded-md cursor-pointer transition-all duration-300 ease-in-out bg-black py-1">Login/Register</button>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default Header
