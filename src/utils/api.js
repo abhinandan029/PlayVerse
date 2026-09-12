386738a9923ff0fa2ee4ddf59a3bd881e2a547da
Abhinandan Manakapure <abhinandanaj29@gmail.com>
2026-09-12 11:14:14 +0530

    Bug fixes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const backendUrl = (
-  import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000'
+  process.env.VITE_BACKEND_URL || 'http://localhost:3000'
 ).replace(/\/$/, '')
 
 export function apiUrl(path) {
